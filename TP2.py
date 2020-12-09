7184dba8f8add9b0cbc6d1c15ae0f01890e3f337
zinab <zinab.saadane@gmail.com>
2020-12-09 18:01:51 +0100

    Commit TP2

---
+++ TP2.py
@@ -0,0 +1,17 @@
+from dateline import dateline
+
+now =  dateline.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
+
+print "Date :" + mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
